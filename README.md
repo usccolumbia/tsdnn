31d527f177391b4866393f7aaeefb58756ae1c0b
Jianjun Hu <jianjunh@cse.sc.edu>
2021-12-11 20:18:17 -0500

    new

---
+++ README.md
@@ -0,0 +1,148 @@
+# Deep Semi-Supervised Teacher-Student Material Synthesizability Prediction
+
+This software package implements the Meta Pseudo Labels (MPL) semi-supervised learning method with Crystal Graph Convolutional Neural Networks (CGCNN) with that takes an arbitary crystal structure to predict material synthesizability.
+
+The package provides two major functions:
+
+- Train a semi-supervised TSDNN model with a customized dataset.
+- Predict material synthesizability of new crystals with a pre-trained TSDNN model.
+
+The following paper describes the details of the CGCNN architecture:
+
+[Crystal Graph Convolutional Neural Networks for an Accurate and Interpretable Prediction of Material Properties](https://link.aps.org/doi/10.1103/PhysRevLett.120.145301)
+
+The following paper describes the details of the semi-supervised learning framework:
+
+[Meta Pseudo Labels](https://arxiv.org/abs/2003.10580)
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Usage](#usage)
+  - [Define a customized dataset](#define-a-customized-dataset)
+  - [Train a TSDNN model](#train-a-TSDNN-model)
+  - [Predict material properties with a pre-trained TSDNN model](#predict-material-properties-with-a-pre-trained-TSDNN-model)
+- [Data](#data)
+- [Authors](#authors)
+- [License](#license)
+
+
+##  Prerequisites
+
+This package requires:
+
+- [PyTorch](http://pytorch.org)
+- [scikit-learn](http://scikit-learn.org/stable/)
+- [pymatgen](http://pymatgen.org)
+
+*Note: this code is tested for PyTorch v1.0.0+ and is not compatible with versions below v0.4.0 due to some breaking changes.
+
+## Usage
+
+### Define a customized dataset 
+
+To input crystal structures to TSDNN, you will need to define a customized dataset. Note that this is required for both training and predicting. 
+
+Before defining a customized dataset, you will need:
+
+- [CIF](https://en.wikipedia.org/wiki/Crystallographic_Information_File) files recording the structure of the crystals that you are interested in
+- The target label for each crystal (not needed for predicting, but you need to put some random numbers in `id_prop.csv`)
+
+You can create a customized dataset by creating a directory `root_dir` with the following files: 
+
+1. `data_labeled.csv`: a [CSV](https://en.wikipedia.org/wiki/Comma-separated_values) file with two columns. The first column recodes a unique `ID` for each crystal, and the second column recodes the known value of the target label. 
+
+1. `data_unlabeled.csv`: a [CSV](https://en.wikipedia.org/wiki/Comma-separated_values) file with two columns. The first column recodes a unique `ID` for each crystal, and the second column can be filled with alternating 1 and 0 (the second column is still needed).
+
+2. `atom_init.json`: a [JSON](https://en.wikipedia.org/wiki/JSON) file that stores the initialization vector for each element. An example of `atom_init.json` is `data/sample-regression/atom_init.json`, which should be good for most applications.
+
+3. `ID.cif`: a [CIF](https://en.wikipedia.org/wiki/Crystallographic_Information_File) file that recodes the crystal structure, where `ID` is the unique `ID` for the crystal.
+
+(4.) `data_predict`: a [CSV](https://en.wikipedia.org/wiki/Comma-separated_values) file with two columns. The first column recodes a unique `ID` for each crystal, and the second column can be filled with alternating 1 and 0 (the second column is still needed). 
+This is the file that will be used if you want to predict material properties with `predict.py`. 
+
+The structure of the `root_dir` should be:
+
+```
+root_dir
+├── data_labeled.csv
+├── data_unlabeled.csv
+├── data_test.csv
+├── data_positive.csv (optional- for positive and unlabeled dataset generation)
+├── data_unlabeled_full.csv (optional- for positive and unlabeled dataset generation, data_unlabeled.csv will be overwritten)
+├── atom_init.json
+├── id0.cif
+├── id1.cif
+├── ...
+```
+
+There is an example of customized a dataset in: `data/example`.
+
+### Train a TSDNN model
+
+Before training a new TSDNN model, you will need to:
+
+- [Define a customized dataset](#define-a-customized-dataset) at `root_dir` to store the structure-property relations of interest.
+
+Then, in directory `synth-tsdnn`, you can train a TSDNN model for your customized dataset by:
+
+```bash
+python main.py root_dir
+```
+
+If you want to use the PU learning dataset generation, you can train a model using the `--uds` flag with the number of PU iterations to perform.
+
+```bash
+python main.py --uds 5 root_dir
+``` 
+
+
+You can set the number of training, validation, and test data with labels `--train-size`, `--val-size`, and `--test-size`. Alternatively, you may use the flags `--train-ratio`, `--val-ratio`, `--test-ratio` instead. Note that the ratio flags cannot be used with the size flags simultaneously. For instance, `data/example` has 10 data points in total. You can train a model by:
+
+```bash
+python main.py --train-size 6 --val-size 2 --test-size 2 data/example
+```
+or alternatively
+```bash
+python main.py --train-ratio 0.6 --val-ratio 0.2 --test-ratio 0.2 data/example
+```
+
+After training, you will get 5 files in `synth-tsdnn` directory.
+
+- `checkpoints/teacher_best.pth.tar`: stores the TSDNN teacher model with the best validation accuracy.
+- `checkpoints/student_best.pth.tar`" stores the TSDNN student model with the best validation accuracy.
+- `checkpoints/t_checkpoint.pth.tar`: stores the TSDNN teacher model at the last epoch.
+- `checkpoints/s_checkpoint.pth.tar`: stores the TSDNN student model at the last epoch.
+- `results/validation/test_results.csv`: stores the `ID` and predicted value for each crystal in training set.
+
+### Predict material properties with a pre-trained TSDNN model
+
+Before predicting the material properties, you will need to:
+
+- [Define a customized dataset](#define-a-customized-dataset) at `root_dir` for all the crystal structures that you want to predict.
+- Obtain a pre-trained TSDNN model (example found in checkpoints/pre-trained/pre-train.pth.tar).
+
+Then, in directory `synth-tsdnn`, you can predict the properties of the crystals in `root_dir`:
+
+```bash
+python predict.py checkpoints/pre-trained/pre-trained.pth.tar data/root_dir
+```
+
+After predicting, you will get one file in `synth-tsdnn` directory:
+
+- `predictions.csv`: stores the `ID` and predicted value for each crystal in test set.
+
+## Data
+
+To reproduce our paper, you can download the corresponding datasets following the [instruction](data/material-data). Each dataset discussed can be found in `data/datasets/`
+
+## Authors
+
+This software was primarily written by [Daniel Gleaves](http://mleg.cse.sc.edu/people.html) who was advised by [Prof. Jianjun Hu](https://cse.sc.edu/~jianjunh/index.html). This software builds upon work by [Tian Xie](https://github.com/txie-93), [Hieu Pham](https://github.com/google-research/google-research/tree/master/meta_pseudo_labels), and [Jungdae Kim](https://github.com/kekmodel).
+
+## License
+
+TSDNN is released under the MIT License.
+
+
+
---
+++ main.py
@@ -0,0 +1,165 @@
+import argparse
+import operator
+import random
+import pandas as pd
+import numpy as np
+import cgcnn.train as t
+import sys
+import csv
+import torch
+import os
+
+parser = argparse.ArgumentParser(description='Semi-Supervised Crystal Graph Convolutional Neural Networks')
+parser.add_argument('data_options', metavar='OPTIONS', nargs='+', help='dataset options, started with the path to root dir, then other options')
+parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+parser.add_argument('--uds', '--udsteps', default=0, type=int, metavar='N', help='number of unsupervised PU learning iterations to perform')
+parser.add_argument('-j', '--workers', default=0, type=int, metavar='N', help='number of data loading workers (default: 0)')
+parser.add_argument('-g', '--gpu', default=0, type=int, metavar='N', help='gpu to run on (default: 0)')
+parser.add_argument('--teacher', default='', type=str, metavar='PATH', help='path to latest teacher checkpoint (default: none)')
+parser.add_argument('--student', default='', type=str, metavar='PATH', help='path to latest student checkpoint (default: none)')
+parser.add_argument('--epochs', default=50, type=int, metavar='N', help='number of total epochs to run (default: 30)')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int, metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float, metavar='LR', help='initial learning rate (default: 0.01)')
+parser.add_argument('--lr-milestones', default=[100], nargs='+', type=int, metavar='N', help='milestones for scheduler (default: [100])')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=0, type=float, metavar='W', help='weight decay (default: 0)')
+parser.add_argument('--print-freq', '-p', default=10, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--iter', default=0, type=int, metavar='N', help='iteration number to save as (default=0)')
+train_group = parser.add_mutually_exclusive_group()
+train_group.add_argument('--train-ratio', default=0.7, type=float, metavar='N', help='number of training data to be loaded (default none)')
+train_group.add_argument('--train-size', default=None, type=int, metavar='N', help='number of training data to be loaded (default none)')
+valid_group = parser.add_mutually_exclusive_group()
+valid_group.add_argument('--val-ratio', default=0.1, type=float, metavar='N', help='percentage of validation data to be loaded (default 0.1)')
+valid_group.add_argument('--val-size', default=None, type=int, metavar='N', help='number of validation data to be loaded (default 1000)')
+test_group = parser.add_mutually_exclusive_group()
+test_group.add_argument('--test-ratio', default=0.2, type=float, metavar='N', help='percentage of test data to be loaded (default 0.1)')
+test_group.add_argument('--test-size', default=None, type=int, metavar='N', help='number of test data to be loaded (default 1000)')
+parser.add_argument('--optim', default='SGD', type=str, metavar='SGD', help='choose an optimizer, SGD or Adam, (default: SGD)')
+parser.add_argument('--atom-fea-len', default=90, type=int, metavar='N', help='number of hidden atom features in conv layers')
+parser.add_argument('--h-fea-len', default=180, type=int, metavar='N', help='number of hidden features after pooling')
+parser.add_argument('--n-conv', default=3, type=int, metavar='N', help='number of conv layers')
+parser.add_argument('--n-h', default=1, type=int, metavar='N', help='number of hidden layers after pooling')
+
+args = parser.parse_args(sys.argv[1:])
+
+args.cuda = not args.disable_cuda and torch.cuda.is_available()
+
+root_dir = args.data_options[0]
+true_negative = 0
+
+
+def main():
+    if args.workers > 1:
+        torch.multiprocessing.set_sharing_strategy('file_system')
+    if args.cuda:
+        torch.cuda.set_device(args.gpu)
+
+    if args.uds:
+        gen_datasets()
+
+        for i in range(args.uds):
+            args.uds = i
+            t.main(args)
+            gen_datasets()
+            # regen_datasets()
+            args.teacher = ''
+            args.student = ''
+    else:
+        t.main(args)
+
+
+def gen_datasets():
+    assert os.path.isfile(root_dir+'/data_positive.csv'), 'missing positive dataset'
+    assert os.path.isfile(root_dir+'/data_unlabeled_full.csv'), 'missing unlabeled dataset'
+
+    positive = pd.read_csv(root_dir+'/data_positive.csv', header=None)
+    positive = positive.iloc[:,0]    # ensure only labels are included
+    unlabeled = pd.read_csv(root_dir+'/data_unlabeled_full.csv', header=None)
+    unlabeled = unlabeled.iloc[:,0]   # ensure only labels are included
+
+    p_size = positive.size
+    u_size = unlabeled.size
+    assert p_size < u_size, 'dataset is unbalanced, more positive samples than unlabeled'
+
+    p_to_u_size = int(p_size*0.3)
+    p_size -= p_to_u_size
+
+    p_labeled = positive.sample(n=p_size).copy().reset_index(drop=True)
+    common = pd.Series(list(set(positive).intersection(set(p_labeled))))
+    p_unlabeled = positive[~positive.isin(common)].copy().reset_index(drop=True)    # remove duplicates
+
+    negative = unlabeled.sample(n=p_size).copy().reset_index(drop=True)
+    common = pd.Series(list(set(unlabeled).intersection(set(negative))))
+    unlabeled = unlabeled[~unlabeled.isin(common)].copy().reset_index(drop=True)
+    unlabeled = pd.concat([unlabeled, p_unlabeled]).reset_index(drop=True)
+    unlabeled = pd.concat([unlabeled, pd.Series([x % 2 for x in range(unlabeled.size)])], axis=1)
+    # print(unlabeled)
+
+    p_labeled = pd.concat([p_labeled, pd.Series([1 for _ in range(p_labeled.size)])], axis=1)
+    negative = pd.concat([negative, pd.Series([0 for _ in range(negative.size)])], axis=1)
+    labeled = pd.concat([p_labeled, negative]).reset_index(drop=True)
+    # print(labeled)
+
+    labeled.to_csv(root_dir+'/data_labeled.csv', header=False, index=False)
+    unlabeled.to_csv(root_dir+'/data_unlabeled.csv', header=False, index=False)
+
+
+def regen_datasets():
+    with open(root_dir+'/data_positive.csv', 'r') as f:
+        reader = csv.reader(f)
+        positive = list(reader)
+        p_size = len(positive)
+
+    with open(f'results/validation/test_results_{args.iter}.csv', 'r') as f:
+        reader = csv.reader(f)
+        results = dict(reader)
+
+    with open(root_dir+f'/data_unlabeled_full.csv', 'r') as f:
+        reader = csv.reader(f)
+        full_unlabeled = [row[0] for row in reader]
+
+    with open(root_dir+f'/data_unlabeled.csv', 'r') as f:
+        reader = csv.reader(f)
+        current_unlabeled = [row[0] for row in reader]
+
+    unlabeled_results = dict((cid, results[cid]) for cid in full_unlabeled)
+    full_unlabeled_sorted = sorted(unlabeled_results, key=unlabeled_results.get)
+    current_unlabeled_sorted = [cid for cid in full_unlabeled_sorted.keys() if cid in current_unlabeled]
+    current_unlabeled_results = [full_unlabeled_sorted[cid] for cid in current_unlabeled]
+    current_negative = sum(pred <= 0.5 for pred in current_unlabeled_results)
+
+    if current_negative >= p_size:
+        new_negative = [(cid, 0) for cid in current_unlabeled_sorted[:p_size]]
+        other_unlabeled = [cid for cid in full_unlabeled_sorted.keys() if cid not in current_unlabeled_sorted[:p_size]]
+        new_unlabeled = [(cid, i%2) for i, cid in enumerate(other_unlabeled)]
+    else:
+        needed = p_size - current_negative
+        unlabeled_negative = [(cid, 0) for cid in current_unlabeled_sorted[:current_negative]]
+        labeled_negative = random.sample()
+        unlabeled = [row[0] for row in reader]
+
+    unlabeled_results = dict((cid, results[cid]) for cid in unlabeled)
+    unlabeled_sorted = sorted(unlabeled_results, key=unlabeled_results.get)
+
+    with open(root_dir+'/data_positive.csv', 'r') as f:
+        reader = csv.reader(f)
+        positive = list(reader)
+
+    p_size = len(positive)
+    new_negative = [(cid, 0) for cid in unlabeled_sorted[:p_size]]
+    new_labeled = positive + new_negative
+    new_unlabeled = [(cid, i%2) for i, cid in enumerate(full_unlabeled_sorted[p_size:])]
+
+    with open(root_dir+'/data_labeled.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(new_labeled)
+
+    with open(root_dir+'/data_unlabeled.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(new_unlabeled)
+
+
+
+if __name__ == '__main__':
+    main()
